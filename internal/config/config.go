4b952b967417120695797bf2480a2f72e62d6109
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-10-27 15:53:13 -0200

    fix

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,9 +4,9 @@ import "github.com/caarlos0/env"
 
 // Config config struct
 type Config struct {
-	User            string   `env:"LIBRATO_EMAIL,required"`
-	Token           string   `env:"LIBRATO_TOKEN,required"`
-	URL             string   `env:"HYSTRIX_URL,required"`
+	User            string   `env:"LIBRATO_EMAIL"`
+	Token           string   `env:"LIBRATO_TOKEN"`
+	URL             string   `env:"HYSTRIX_URL"`
 	Clusters        []string `env:"HYSTRIX_CLUSTERS"`
 	ReportLatencies []string `env:"HYSTRIX_REPORT_LATENCIES" envDefault:"100th,99.5th,99th,95th,90th,75th,50th,25th,0th,mean"`
 	ReportInterval  int      `env:"HYSTRIX_REPORT_INTERVAL" envDefault:"5"`
